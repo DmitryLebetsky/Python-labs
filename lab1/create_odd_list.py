0292a7f201222a6c6ba6a164f4fda39182f9ab6b
Dmitry Lebetsky <dlebetsky@gmail.com>
2023-03-09 12:21:23 +0300

    add creat_odd_list.py

---
+++ lab1/create_odd_list.py
@@ -0,0 +1,23 @@
+from constants import STOP_VALUE
+
+
+def create_odd_list():
+    should_stop = False
+    full_list = []
+    odd_list = []
+    while not should_stop:
+        entered_number = input("Next element: ")
+        if entered_number == STOP_VALUE:
+            should_stop = True
+            continue
+        try:
+            entered_number = int(entered_number)
+            full_list.append(entered_number)
+        except ValueError:
+            print("This is not integer! Try again :)")
+    print("List was created: " + str(full_list))
+    for number in full_list:
+        if number % 2 == 1:
+            odd_list.append(number)
+    return odd_list
+
--- lab1/main.py
+++ lab1/main.py
@@ -1,10 +1,17 @@
 from hello_func import hello_world
 from calculate_func import calculate
+from create_odd_list import create_odd_list
 
 hello_world()
 
+print()
 first_operand = input("Enter first operand: ")
 second_operand = input("Enter second operand: ")
 operator = input("Enter operand (add, sub, mult, div): ")
 calculate(first_operand, second_operand, operator)
+
+print()
+print("Create your list of integer values. Press enter to stop filling it")
+odd_list = create_odd_list()
+print("Odd list: " + str(odd_list))
 
