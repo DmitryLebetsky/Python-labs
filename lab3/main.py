b00219206a767a3ab31c3f262ad75c76d18e3179
Dmitry Lebetsky <dlebetsky@gmail.com>
2023-06-07 01:55:38 +0300

    feat: add lab3

--- lab3/main.py
+++ lab3/main.py
@@ -1,15 +1,44 @@
-from object_serializer import SerializerFactory
-class Hello:
-    @classmethod
-    def test_classmethod(cls):
-        return cls.__test
+from SerizDeseriz_IGI import SerializerFactory
+
+
+def int_wrap_decor(func):
+    def wrapper(arg):
+        if isinstance(arg, int):
+            arg += 3
+            print(arg)
+        else:
+            print("I am not int")
+        return func(arg)
+
+    return wrapper
+
+@int_wrap_decor
+def check_int(maybe_int):
+    print("Passed value = " + str(maybe_int))
+
+check_int(5)
+check_int("blablabla")
+
+def my_decor(func):
+    def inner(*args, **kwargs):
+        print("Hello")
+        return func(*args, **kwargs)
+    return inner
+
+class A:
+    @my_decor
+    def my_method(self):
+        return 145
+class B:
+    pass
+
+class C(A, B):
+    pass
 
 
 serializer = SerializerFactory.create_serializer("JSON")
 
-ser = serializer.dumps(Hello)
-print(ser)
+ser = serializer.dumps(C)
 
 des = serializer.loads(ser)
 
-instance = des()
